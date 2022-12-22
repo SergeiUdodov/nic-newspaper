eb5020c1fcbb2de122e7581d9f96fedf12ec9c69
sergei <s_udodov@bk.ru>
2022-12-22 20:27:00 +0300

    initial commit

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.luv2code.springsecurity.demo.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// add a reference to our user service
+    @Autowired
+    private UserService userService;
+	
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    
+   @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+			.antMatchers("/").hasRole("EMPLOYEE") // any EMPLOYEE has access to / path and all subdirs
+			.antMatchers("/leaders/**").hasRole("MANAGER") // only MANAGER has access to /leaders/ path and all subdirs
+			.antMatchers("/systems/**").hasRole("ADMIN") // only ADMIN has access to /systems/ path and all subdirs
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage") //in LoginController
+				.loginProcessingUrl("/authenticateTheUser") //no controller required, spring will handle it for free(submit in fancy-login)
+				.successHandler(customAuthenticationSuccessHandler)
+				.permitAll()
+			.and()
+			.logout().permitAll() //adding logout support, no coding required(invalidates session and removes cookies, send to login page and append ?logout parameter)
+			/*.and()
+			.exceptionHandling().accessDeniedPage("/access-denied")*/; // we don't use it here because John for example doesn't even see links /leaders and /systems (check home.html)
+		
+	}
+	
+	//beans
+	//bcrypt bean definition
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	//authenticationProvider bean definition
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(userService); //set the custom user details service
+		auth.setPasswordEncoder(passwordEncoder()); //set the password encoder - bcrypt
+		return auth;
+	}
+	  
+}
+
+
+
+
+
+
